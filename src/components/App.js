a4f60af58736c20aa31353217bc011f5d7b8a27a
Abhishek Anand <yeyeabhishekanand.com>
2021-02-20 22:45:14 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+  <h1 data-ns-test="project-name">Oyo Clone</h1>
+  <h1 data-ns-test ="project-description">vjdfjgvkndfvfdnnnvndfbjg fvfjvn vvnfvjfjskdsjk ddnncf jvnfjf</h1>
+    </div>
   )
 }
 
